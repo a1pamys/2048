7916918f7c117997fc09c57c73e335553bc8a6c7
Alpamys Duimagambetov <dalpamysn@gmail.com>
2023-04-30 19:26:19 +0700

    add move pieces

--- 2048.py
+++ 2048.py
@@ -35,6 +35,7 @@ board_values = [[0 for _ in range(4)] for _ in range(4)]
 spawn_new = True
 init_count = 0
 score = 0
+direction = ''
 
 # draw tiles for game
 def draw_pieces(board):
@@ -60,6 +61,84 @@ def draw_pieces(board):
                 text_rect = value_text.get_rect(center=(j * 95 + 57, i * 95 + 157))
                 screen.blit(value_text, text_rect)
                 pygame.draw.rect(screen, 'black', [j * 95 + 20, i * 95 + 120, 75, 75], 2, 5)
+
+# take your turn based on direction
+def take_turn(direc, board):
+    global score
+    merged = [[False for _ in range(4)] for _ in range(4)]
+    if direc == 'UP':
+        for i in range(4):
+            for j in range(4):
+                shift = 0
+                if i > 0:
+                    for q in range(i):
+                        if board[q][j] == 0:
+                            shift += 1
+                    if shift > 0:
+                        board[i - shift][j] = board[i][j]
+                        board[i][j] = 0
+
+                    if board[i - shift - 1][j] == board[i - shift][j] and not merged[i - shift][j] \
+                            and not merged[i - shift - 1][j]:
+                        board[i - shift - 1][j] *= 2
+                        score += board[i - shift - 1][j]
+                        board[i - shift][j] = 0
+                        merged[i - shift - 1][j] = True
+
+    elif direc == 'DOWN':
+        for i in range(3):
+            for j in range(4):
+                shift = 0
+                for q in range(i + 1):
+                    if board[3 - q][j] == 0:
+                        shift += 1
+                if shift > 0:
+                    board[2 - i + shift][j] = board[2 - i][j]
+                    board[2 - i][j] = 0
+                if 3 - i + shift <= 3:
+                    if board[2 - i + shift][j] == board[3 - i + shift][j] and not merged[3 - i + shift][j] \
+                            and not merged[2 - i + shift][j]:
+                        board[3 - i + shift][j] *= 2
+                        score += board[3 - i + shift][j]
+                        board[2 - i + shift][j] = 0
+                        merged[3 - i + shift][j] = True
+
+    elif direc == 'LEFT':
+        for i in range(4):
+            for j in range(4):
+                shift = 0
+                for q in range(j):
+                    if board[i][q] == 0:
+                        shift += 1
+                if shift > 0:
+                    board[i][j - shift] = board[i][j]
+                    board[i][j] = 0
+                if board[i][j - shift] == board[i][j - shift - 1] and not merged[i][j - shift - 1] \
+                        and not merged[i][j - shift]:
+                    board[i][j - shift - 1] *= 2
+                    score += board[i][j - shift - 1]
+                    board[i][j - shift] = 0
+                    merged[i][j - shift - 1] = True
+
+    elif direc == 'RIGHT':
+        for i in range(4):
+            for j in range(4):
+                shift = 0
+                for q in range(j):
+                    if board[i][3 - q] == 0:
+                        shift += 1
+                if shift > 0:
+                    board[i][3 - j + shift] = board[i][3 - j]
+                    board[i][3 - j] = 0
+                if 4 - j + shift <= 3:
+                    if board[i][4 - j + shift] == board[i][3 - j + shift] and not merged[i][4 - j + shift] \
+                            and not merged[i][3 - j + shift]:
+                        board[i][4 - j + shift] *= 2
+                        score += board[i][4 - j + shift]
+                        board[i][3 - j + shift] = 0
+                        merged[i][4 - j + shift] = True
+    return board
+
 
 # spawn in new pieces randomly when turns start
 def new_pieces(board):
@@ -98,9 +177,26 @@ while run:
         spawn_new = False
         init_count += 1
 
+    if direction != '':
+        board_values = take_turn(direction, board_values)
+        direction = ''
+        spawn_new = True
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                direction = 'UP'
+            elif event.key == pygame.K_DOWN:
+                direction = 'DOWN'
+            elif event.key == pygame.K_LEFT:
+                direction = 'LEFT'
+            elif event.key == pygame.K_RIGHT:
+                direction = 'RIGHT'
 
     pygame.display.flip()
 pygame.quit()
